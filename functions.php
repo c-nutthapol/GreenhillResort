00d7a4030650257c90fe7d4eb338524971791da5
nutthapol-chue <58501595+nutthapol-chue@users.noreply.github.com>
2020-11-24 22:50:39 +0700

    set utf8

--- functions.php
+++ functions.php
@@ -12,20 +12,21 @@
             if(mysqli_connect_errno()){
                 echo "Connection failed: ". mysqli_connect_error();
             }
+            $this->dbcon->set_charset("utf8");
         }
 
         public function usernameavaiable($uname){
-            $checkuser = mysqli_query($this->dbcon, "SELECT username FROM tbluser WHERE username = '$uname' ");
+            $checkuser = mysqli_query($this->dbcon, "SELECT username FROM tbluser WHERE username = '".$uname."' ");
             return $checkuser;
         }
 
         public function registration($fname, $uname, $uemail, $password){
-            $reg = mysqli_query($this->dbcon, "INSERT INTO tbluser(fullname, username, useremail, password) VALUES('$fname','$uname','$uemail','$password') ");
+            $reg = mysqli_query($this->dbcon, "INSERT INTO tbluser(fullname, username, useremail, password) VALUES('".$fname."','".$uname."','".$uemail."','".$password."') ");
             return $reg;
         }
 
         public function signin($uname, $password){
-            $signinquery = mysqli_query($this->dbcon, "SELECT id, fullname FROM tbluser WHERE username = '$uname' AND password = '$password' ");
+            $signinquery = mysqli_query($this->dbcon, "SELECT id, fullname FROM tbluser WHERE username = '".$uname."' AND password = '".$password."' ");
             return $signinquery;
         }
     }
